660c0a605d94d5ad05f24d4a6918c862f5764171
Francesco Castellana <xfranky@gmail.com>
2021-01-12 00:12:53 +0800

    More docs

--- src/it/xfnet/mlanon/MLAnonimizedSummary.java
+++ src/it/xfnet/mlanon/MLAnonimizedSummary.java
@@ -13,6 +13,18 @@ class MLAnonimizedSummary {
 	long maxSpeed;
 	double avgSpeed;
 
+	/**
+	* Main constructor, with all levels. Use null when the level didn't have enough data to
+	* be anonymized. For now the aggregated data (average and max speed) have to be set separately
+	* to avoid too many parameters in the constructor; this might be changed later adding a new
+	* constructor if deemed proper.
+	*
+	* @param theCountry a String with the ISO code of the country
+	* @param theRegion	a String with the name of the region
+	* @param theIsp		a String with the name of the ISP
+	* @param theAsn		an Integer with the autonomous system number
+	* @return the summary object
+	*/
 	MLAnonimizedSummary(String theCountry, String theRegion, String theIsp, Integer theAsn){
 		country = theCountry;
 		region = theRegion;
