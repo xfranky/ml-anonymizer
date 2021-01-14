f4e5e0214ef76d53d8cca76efb01fda5131f624c
Francesco Castellana <xfranky@gmail.com>
2021-01-14 23:23:48 +0800

    Changing visibility to the accessory methods and documenting them

--- src/it/xfnet/mlanon/MLResult.java
+++ src/it/xfnet/mlanon/MLResult.java
@@ -1,5 +1,8 @@
 package it.xfnet.mlanon;
 
+/**
+* Class representing a result to be added to the anonymizer, with all its components
+*/
 public class MLResult {
 	long speed;
 	String country;
@@ -7,6 +10,14 @@ public class MLResult {
 	String isp;
 	int asn;
 
+	/**
+	* Constructor for the result objects to be fed to the anonymizer
+	* @param theSpeed the data transfer speed recorded for this result
+	* @param theCountry the country the connection came from
+	* @param theRegion the region in the country the connection came from
+	* @param theIsp the name of the IPS the connection came from
+	* @param theAsn the Autonomous System Number the connection came from
+	*/
 	public MLResult(long theSpeed, String theCountry, String theRegion, String theIsp, int theAsn){
 		speed = theSpeed;
 		country = theCountry;
@@ -15,23 +26,43 @@ public class MLResult {
 		asn = theAsn;
 	}
 
-	String getCountry() {
+	/**
+	* Method to get the country recorded in this result
+	* @return the country of the result 
+	*/
+	public String getCountry() {
 		return country;
 	}
 
-	String getRegion() {
+	/**
+	* Method to get the region recorded in this result
+	* @return the region of the result 
+	*/
+	public String getRegion() {
 		return region;
 	}
 
-	String getIsp() {
+	/**
+	* Method to get the ISP recorded in this result
+	* @return the ISP of the result 
+	*/
+	public String getIsp() {
 		return isp;
 	}
 
-	int getAsn() {
+	/**
+	* Method to get the Autonomous System Number recorded in this result
+	* @return the ASN of the result 
+	*/
+	public int getAsn() {
 		return asn;
 	}
 
-	long getSpeed() {
+	/**
+	* Method to get the transfer speed recorded in this result
+	* @return the transfer speed of the result 
+	*/
+	public long getSpeed() {
 		return speed;
 	}
 }
